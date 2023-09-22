ee10b5abd15ce4bb1d64e24177cf84e6a33603f0
joshua-983 <officialgh983@gmail.com>
2023-09-21 21:33:45 -0700

    pall

--- pall.c
+++ pall.c
@@ -10,14 +10,13 @@ void f_pall(stack_t **head, unsigned int counter)
 	stack_t *h;
 	(void)counter;
 
-	h =*head;
+	h = *head;
 	if (h == NULL)/*Check if the stack is empty (head is NULL).
 */
 		return;
 	while (h)
 	{
 		printf("%d\n", h->n);/*Print the value of the current element.*/
-		h = h->next;/*Move to the next element in the stack.
-        }*/
+		h = h->next;/*Move to the next element in the stack*/
 	}
 }
